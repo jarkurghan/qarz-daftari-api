9682fddbb23d5179c2e58e2dfca210bc09d32a6c
Natural <jarkurghan@gmail.com>
2024-10-20 21:46:08 +0500

    update journal tables

--- src/db/migrations/20241016113910_journal.js
+++ src/db/migrations/20241016113910_journal.js
@@ -11,14 +11,32 @@ export function up(knex) {
         })
         .createTable("journal", function (table) {
             table.increments("id").primary();
-            table.string("name");
-            table.integer("supervisor");
+            table.string("name").notNullable();
+            table.integer("supervisor").notNullable();
             table.foreign("supervisor").references("id").inTable("profile");
+            table.boolean("private").defaultTo(false).notNullable();
+            table.boolean("note").defaultTo(true).notNullable();
             table.integer("created_by").notNullable();
             table.foreign("created_by").references("id").inTable("profile");
-            table.boolean("private").defaultTo(false).notNullable();
-            table.string("default_type").defaultTo("xohlasa tanlamaslik").notNullable();
             table.datetime("created_date").defaultTo(knex.fn.now()).notNullable();
+        })
+        .createTable("journal_value", function (table) {
+            table.increments("id").primary();
+            table.integer("journal_id").notNullable();
+            table.foreign("journal_id").references("id").inTable("journal");
+            table.boolean("folderable").defaultTo(false).notNullable();
+            table.string("debt_type_default").defaultTo("mendan qarz").notNullable();
+            table.boolean("debt_type_required").defaultTo(false).notNullable();
+            table.string("amount_type").defaultTo("float").notNullable();
+            table.string("amount_currency").defaultTo("so'm").notNullable();
+            table.boolean("amount_required").defaultTo(true).notNullable();
+            table.string("date_type").defaultTo("date").notNullable();
+            table.string("date_default_time").defaultTo("19:00").notNullable();
+            table.boolean("date_required").defaultTo(true).notNullable();
+            table.boolean("addressable").defaultTo(false).notNullable();
+            table.boolean("address_required").defaultTo(false).notNullable();
+            table.boolean("phonable").defaultTo(false).notNullable();
+            table.boolean("phone_required").defaultTo(false).notNullable();
         })
         .createTable("journal_profile_access", function (table) {
             table.increments("id").primary();
@@ -39,5 +57,8 @@ export function up(knex) {
  * @returns { Promise<void> }
  */
 export function down(knex) {
-    return knex.schema.dropTable("journal_person_profile_access").dropTable("journal").dropTable("journal_access");
+    return knex.schema.dropTable("journal_profile_access")
+        .dropTable("journal_value")
+        .dropTable("journal")
+        .dropTable("journal_access");
 }
