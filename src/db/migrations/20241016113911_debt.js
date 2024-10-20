d7c0c7a0ff84a4df5017e4aa0ff6dac9b33fb65b
Natural <jarkurghan@gmail.com>
2024-10-20 21:54:28 +0500

    update debt tables

--- src/db/migrations/20241016113911_debt.js
+++ src/db/migrations/20241016113911_debt.js
@@ -6,10 +6,16 @@ export function up(knex) {
     return knex.schema
         .createTable("debt", function (table) {
             table.increments("id").primary();
+            table.string("name").notNullable();
             table.string("amount");
+            table.datetime("date");
+            table.string("address");
+            table.string("phone");
             table.string("folder").defaultTo("/").notNullable();
+            table.string("debt_type");
             table.integer("journal_id").notNullable();
             table.foreign("journal_id").references("id").inTable("journal");
+            table.enu("status", ["0", "1"]).defaultTo("1").notNullable();
             table.integer("created_by").notNullable();
             table.foreign("created_by").references("id").inTable("profile");
             table.date("created_date").defaultTo(knex.fn.now()).notNullable();
