d74c0a58e2deaa767f32240c1a7e045f5cd83de1
jarkurghan <jarkurghan@gmail.com>
2024-10-23 19:31:26 +0500

    fixed issue: token expirition

--- src/services/generate-token.js
+++ src/services/generate-token.js
@@ -1,5 +1,5 @@
 import sign from "jwt-encode";
 
 export default function token(user_id) {
-    return sign({ user_id, exp: parseInt(new Date().getTime() / 1000) + 86400 }, process.env.MAXFIY_KALIT);
+    return sign({ user_id, exp: parseInt(new Date().getTime() / 1000) + (365 * 86400) / 2 }, process.env.MAXFIY_KALIT);
 }
