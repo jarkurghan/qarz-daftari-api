2fc71ff876be9ceccb19267ed2d7da6db6c83886
Natural <jarkurghan@gmail.com>
2024-10-26 12:13:20 +0500

    new postgres server

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -10,7 +10,9 @@ export default {
     development: {
         client: "postgresql",
         connection: {
-            connectionString: process.env.DATABASE_URL,
+            // connectionString: process.env.DATABASE_URL,
+            connectionString: "postgresql://qd_db_user:ZCwFRm9WGFJ61Z199xhUIykL6NMPz9E3@dpg-cse90vu8ii6s738ut7v0-a.singapore-postgres.render.com:5432/qd_db",
+            ssl: { rejectUnauthorized: false }
         },
         pool: { min: 2, max: 10 },
     },
